f53b31e320283cc28cd041b666178adc49bd1afb
Lautaro Bustos Raiz <lautaro.b.r96@gmail.com>
2020-11-16 14:22:33 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatPaginatorModule} from '@angular/material/Paginator';
+import {MatProgressBarModule  } from '@angular/material/Progress-bar';
+import {MatSnackBarModule  } from '@angular/material/Snack-Bar';
+import {MatSortModule} from '@angular/material/Sort';
+
+import { MatTableModule} from '@angular/material/Table';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { LoginComponent } from './component/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
